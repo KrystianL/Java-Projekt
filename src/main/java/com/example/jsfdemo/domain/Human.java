eddd94f5260a401c528b980b9302bcacdf8afb96
student <student@student-VirtualBox>
2013-01-23 16:39:44 +0100

    JSF przerobiony

---
+++ src/main/java/com/example/jsfdemo/domain/Human.java
@@ -0,0 +1,38 @@
+package com.example.jsfdemo.domain;
+
+import java.util.List;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class Human {
+	
+	private String Name = "unknown";
+	private String Pesel = "";
+	
+	@Size(min = 2, max = 20)
+	public String getName() {
+		return Name;
+	}
+
+	
+	
+	public void setName(String Name) {
+		this.Name = Name;
+	}
+	
+	@Pattern(regexp = "[0-9]")
+	public String getPesel() {
+		return Pesel;
+	}
+	public void setPesel(String Pesel) {
+		this.Pesel = Pesel;
+	}
+	public void setWrappedData(List<Human> allHumans) {
+		
+		
+	}
+
+	
+	
+}
---
+++ src/main/java/com/example/jsfdemo/service/HumanManager.java
@@ -0,0 +1,41 @@
+package com.example.jsfdemo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import com.example.jsfdemo.domain.Human;
+
+
+@ApplicationScoped
+public class HumanManager {
+	private List<Human> db = new ArrayList<Human>();
+
+	public void addHuman(Human human) {
+		Human newHuman = new Human();
+
+		newHuman.setName(human.getName());
+		newHuman.setPesel(human.getPesel());
+	
+
+		db.add(newHuman);
+	}
+
+	// usuwa humana z wybranym peselem
+	public void deleteHuman(Human human) {
+		Human humanToRemove = null;
+		for (Human p : db) {
+			if (human.getPesel().equals(p.getPesel())) {
+				humanToRemove = p;
+				break;
+			}
+		}
+		if (humanToRemove != null)
+			db.remove(humanToRemove);
+	}
+
+	public List<Human> getAllHumans() {
+		return db;
+	}
+}
